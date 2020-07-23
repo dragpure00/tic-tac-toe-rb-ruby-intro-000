20db7c04755511af37a937a8f2a68713b7985954
Ayaz Araf <ayaz.araf@gmail.com>
2020-07-23 04:36:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -157,7 +157,7 @@ def play(board)
   if won?(board)
     winner(board)=="X"|| winner(board)=="O"
     puts "Congratulations #{winner(board)} !"
-  elseif draw?(board)
+  elsif draw?(board)
   puts "Cats Game"
 end
 end
