f3966fcc328bca7112b4c213d334e9e7fb691d8f
Ayaz Araf <ayaz.araf@gmail.com>
2020-07-23 04:29:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -89,3 +89,59 @@ def current_player(board)
     return "O"
   end
 end
+
+def winner(board)
+  index=[]
+  index=won?(board)
+  if index==false
+    return nil
+  else
+    if board[index[0]]== "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
+end
+
+
+def won?(board)
+  WIN_COMBINATIONS.each {|wc|
+  win_index_1=wc[0]
+  win_index_2=wc[1]
+  win_index_3=wc[2]
+
+p1=board[win_index_1]
+p2=board[win_index_2]
+p3=board[win_index_3]
+
+if p1== "X"&&p2=="X"&&p3=="X"
+  return wc
+elsif p1== "O"&&p2=="O"&&p3=="O"
+  return wc
+end
+}
+return false
+end
+
+def full?(board)
+  board.all? {|index| index == "X" || index=="O" }
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+
+
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
+    return true
+  else
+    return false
+  end
+end
